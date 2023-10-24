2d9b724e522beeff7ae0842b70ef8c63c8636b02
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-24 11:55:05 +0400

    Determined movement direction

--- App.js
+++ App.js
@@ -71,21 +71,27 @@ const stepThroughCell = (row, column) => {
 
   // Assemble randomly-ordered list of neighbors
   const neighbors = shuffle([
-    [row - 1, column],
-    [row, column + 1],
-    [row + 1, column],
-    [row, column - 1]
+    [row - 1, column, 'up'],
+    [row, column + 1, 'right'],
+    [row + 1, column, 'down'],
+    [row, column - 1, 'left']
   ]);
  
 
   // For each neighbor....
-
+  for ( let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
   // See if that neighbor is out of bounds
-
+  if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+    continue;
+  }
   // If we have visited that neighbor, continue to next neighbor
-
+  if (grid[nextRow][nextColumn]){
+    continue;
+  }
   // Remove a wall from either horizontals or verticals
-
+  
+}
   // Visit that next cell
 };
 

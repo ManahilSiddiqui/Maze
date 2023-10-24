c5b533e0fde88287e02055fae5ac041b03246fc7
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-24 11:47:58 +0400

    Shuffling neighbor pairs

--- App.js
+++ App.js
@@ -29,6 +29,22 @@ World.add(world, walls);
 
 // Maze generation
 
+const shuffle = arr => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -37,7 +53,7 @@ const verticals = Array(cells)
   .fill(null)
   .map(() => Array(cells - 1).fill(false));
 
-const horizontals = Array(cells- 1)
+const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
@@ -45,27 +61,32 @@ const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCell = (row, column) => {
- // if i have visted the cell at [row, column], then return 
- if (grid[row][column]) {
+  // If i have visted the cell at [row, column], then return
+  if (grid[row][column]) {
     return;
   }
 
-  // Mark this cell as bring visited 
-  grid[row] [column] = true;
+  // Mark this cell as being visited
+  grid[row][column] = true;
 
   // Assemble randomly-ordered list of neighbors
-  const neighbors = [
+  const neighbors = shuffle([
     [row - 1, column],
     [row, column + 1],
     [row + 1, column],
     [row, column - 1]
-  ];
-  // For each neighbor...
+  ]);
+ 
 
-  // See if the neighbor is out of bounds
+  // For each neighbor....
 
-  // if we have visted that neighbor, continue to the next neigbor
+  // See if that neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either horizontals or verticals
+
+  // Visit that next cell
 };
 
-stepThroughCell(startRow, startColumn);
-console.log(grid);
+stepThroughCell(1, 1);
